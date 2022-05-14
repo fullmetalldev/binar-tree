63487ea1a88ac4afde651555d62eec792e037a71
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-05-14 18:33:57 +0600

    added button

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -7,26 +7,31 @@ const Main = () => {
 
     }, []);
 
+    const addBin = () => {
+        const binars = document.querySelectorAll('.binar');
+
+        let bin = document.createElement('div');
+        bin.textContent = Math.floor(Math.random() * (100 - (-100)) + (-100));
+        if (+bin.textContent > +binars[binars.length - 1].textContent) {
+            bin.className = "binar right"
+        } else {
+            bin.className = "binar left"
+        }
+        binars[binars.length - 1].append(bin)
+    };
 
     const numberChange = async (event) => {
-
         if (event.keyCode === 32) {
-            const binars = document.querySelectorAll('.binar');
-
-            let bin = document.createElement('div');
-            bin.textContent = Math.floor(Math.random() * (100 - (-100)) + (-100));
-            if (+bin.textContent > +binars[binars.length-1].textContent) {
-                bin.className = "binar right"
-            } else {
-                bin.className = "binar left"
-            }
-            binars[binars.length - 1].append(bin)
+            addBin()
         }
     };
 
 
     return (
         <div className="Main">
+            <button onClick={()=>{
+                addBin()
+            }}>Add</button>
             <div className="binar mainBin">
                 0
             </div>
